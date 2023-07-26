a55ef8c1850a0f2159aed928741f04867145f125
sihamerraki <siham95er@gmail.com>
2023-07-25 17:44:01 -0700

    printf

--- main.h
+++ main.h
@@ -19,7 +19,7 @@
 #define S_SHORT 1
 
 /**
- * struct fmt - Struct 
+ * struct fmt - Struct op
  *
  * @fmt: The format.
  * @fn: The function associated.
@@ -27,7 +27,7 @@
 struct fmt
 {
 	char fmt;
-	int (*fn)(va_list, char[], int, int, int);
+	int (*fn)(va_list, char[], int, int, int, int);
 };
 
 
@@ -74,11 +74,11 @@ char buffer[], int flags, char flag_ch, int width, int precision, int size);
 int print_non_printable(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
-/* Function to print memory address */
+/* Funcion to print memory address */
 int print_pointer(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
-/* Functions to handle other specifiers */
+/* Funciotns to handle other specifiers */
 int get_flags(const char *format, int *i);
 int get_width(const char *format, int *i, va_list list);
 int get_precision(const char *format, int *i, va_list list);
@@ -88,7 +88,7 @@ int get_size(const char *format, int *i);
 int print_reverse(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
-/*Function to print a string in root*/
+/*Function to print a string in rot 13*/
 int print_rot13string(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
@@ -114,4 +114,5 @@ int is_digit(char);
 long int convert_size_number(long int num, int size);
 long int convert_size_unsgnd(unsigned long int num, int size);
 
-#endif
+#endif /* MAIN_H */
+
